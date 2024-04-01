94350405dfe70faa35fc76fe6d7b498010cae850
Raul Zarza <raulzarza.dev@gmail.com>
2024-04-01 14:59:04 -0700

    add date libs

---
+++ src/libs/expireDate.ts
@@ -0,0 +1,74 @@
+import { Timestamp } from 'firebase/firestore'
+import { PriceType, TimePriceType } from '../types/PriceType'
+import { addDays, addHours, addMinutes, addMonths, addWeeks } from 'date-fns'
+import asDate from './utils-date'
+/**
+ *
+ * @param time its a string with the time and the unit of time
+ * @returns the time in seconds
+ */
+export function priceTimeInSeconds(time: TimePriceType): number {
+  const timeArray = time.split(' ')
+  const timeNumber = parseInt(timeArray[0])
+  const timeUnit = timeArray[1]
+  const units = {
+    second: 1,
+    minute: 60,
+    hour: 3600,
+    day: 86400,
+    week: 604800,
+    month: 2628000,
+    year: 31536000
+  }
+  return timeNumber * units[timeUnit]
+}
+
+export default function expireDate(
+  time: TimePriceType,
+  startedAt: Date | Timestamp,
+  price?: Partial<PriceType>,
+  priceQty?: number
+): Date | null {
+  if (!price) return startedAt as Date
+  const startedAtDate = asDate(startedAt)
+  if (startedAtDate === null) return null
+  const [qty, unit] = price?.time?.split(' ') || ['', '']
+  const QTY = parseInt(qty) * (priceQty || 1)
+  if (unit === 'year') {
+    const expireDate = addMonths(startedAtDate, QTY * 12)
+    return expireDate
+  }
+  if (unit === 'hour') {
+    const expireDate = addHours(startedAtDate, QTY)
+    return expireDate
+  }
+  if (unit === 'minute') {
+    const expireDate = addMinutes(startedAtDate, QTY)
+
+    return expireDate
+  }
+  if (unit === 'month') {
+    const expireDate = addMonths(startedAtDate, QTY)
+    return expireDate
+  }
+  if (unit === 'week') {
+    const expireDate = addWeeks(startedAtDate, QTY)
+    return expireDate
+  }
+  if (unit === 'day') {
+    const expireDate = addDays(startedAtDate, QTY)
+    return expireDate
+  }
+  console.error('dont unit match')
+  // if time does not exist return null
+  if (!time || !startedAt) return null
+  // if time does not have a number return null
+
+  if (!startedAtDate) return null
+  const timeInSeconds = priceTimeInSeconds(time)
+
+  const expireDate = new Date(
+    asDate(startedAt)?.getTime() + timeInSeconds * 1000
+  )
+  return expireDate
+}
---
+++ src/libs/utils-date.ts
@@ -0,0 +1,111 @@
+import { Timestamp } from 'firebase/firestore'
+import {
+  FormatDistanceToken,
+  endOfWeek,
+  format as fnsFormat,
+  formatDistanceToNowStrict,
+  isWithinInterval,
+  startOfWeek,
+  subWeeks
+} from 'date-fns'
+import { es } from 'date-fns/locale'
+
+export const weekDays = {
+  0: 'Domingo',
+  1: 'Lunes',
+  2: 'Martes',
+  3: 'Miércoles',
+  4: 'Jueves',
+  5: 'Viernes',
+  6: 'Sabado'
+}
+
+export const dateMx = (date?: Date | Timestamp | null) => {
+  if (!date) return '-'
+  const value = date instanceof Timestamp ? date.toDate() : date
+  return new Intl.DateTimeFormat(['es-Mx']).format(value)
+}
+
+export const dateFormat = (
+  date?: number | Date | Timestamp | null,
+  strFormat?: string
+): string => {
+  if (!date) return 'n/d' //* not a date
+  const value = date instanceof Timestamp ? date.toDate() : date
+  const res = fnsFormat(value, strFormat || 'dd/MMM/yy', {
+    locale: es
+  })
+  return res
+}
+
+const customLocale = {
+  ...es,
+  formatDistance: (
+    token: string,
+    count: number,
+    options?: { addSuffix?: boolean; comparison?: number }
+  ) => {
+    const units = {
+      xSeconds: 's',
+      xMinutes: 'm',
+      xHours: 'h',
+      xDays: 'd',
+      xWeeks: 'S',
+      xMonths: 'M'
+    }
+    const unit = units[token]
+    if (!unit) {
+      throw new Error(`Invalid time unit: ${token}`)
+    }
+    const days = count * options.comparison
+    return options?.addSuffix
+      ? days < 0
+        ? 'hace ' + count + ' ' + unit
+        : 'en ' + count + ' ' + unit
+      : count + unit
+  }
+}
+
+export const fromNow = (date?: number | Date | Timestamp | null) => {
+  const validDate = asDate(date)
+  if (!validDate) return '-'
+  const res = formatDistanceToNowStrict(validDate, {
+    locale: customLocale,
+    addSuffix: true,
+    roundingMethod: 'round'
+  })
+  return res
+}
+
+export const inputDateFormat = (
+  date: Date | Timestamp | string | number = new Date()
+) => dateFormat(asDate(date), "yyyy-MM-dd'T'HH:mm")
+
+export const asDate = (
+  date?: Timestamp | Date | number | string | object | null
+): Date | null => {
+  if (!date) return null
+  if (date instanceof Date) {
+    if (!isNaN(date.getTime())) return date
+    console.error('not a valid date', { date })
+    return null
+  }
+  if (date instanceof Timestamp) return date.toDate()
+  if (typeof date === 'number') return new Date(date)
+  if (typeof date === 'string') {
+    const newDate = new Date(date)
+    if (newDate.getTime()) return newDate
+  }
+  console.error('not a valid date', { date })
+  return null
+}
+export default asDate
+
+export function isLastWeek(date) {
+  const lastWeek = {
+    start: startOfWeek(subWeeks(new Date(), 1)),
+    end: endOfWeek(subWeeks(new Date(), 1))
+  }
+
+  return isWithinInterval(date, lastWeek)
+}
