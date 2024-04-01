f92beeedc23f8f452088c4b8b99c826980f0d20f
Raul Zarza <raulzarza.dev@gmail.com>
2024-04-01 09:23:20 -0700

    update shop page

--- src/components/ShopHome.tsx
+++ src/components/ShopHome.tsx
@@ -7,12 +7,22 @@ export default async function ShopHome({ shop }: { shop: Partial<Shop> }) {
   const { name, description, img, id } = shop
   const data = await getShopItems((id as string) || '')
   return (
-    <div>
-      <h1 className="font-bold text-4xl text-center">{name}</h1>
-      <div className="relative w-full aspect-square">
-        <Image src={img || '/forest-image.jpeg'} alt={'Shop Image'} fill />
+    <div className="">
+      <div className="sm:flex my-6">
+        <div className=" ">
+          <Image
+            src={img || '/forest-image.jpeg'}
+            alt={'Shop Image'}
+            width={160}
+            height={160}
+            className="sm:rounded-full rounded-md m-auto shadow-md shadow-black w-full max-w-44   "
+          />
+        </div>
+        <div className="pl-4">
+          <h1 className="font-bold text-4xl text-left">{name}</h1>
+          <p className="text-left my-4 text-lg">{description}</p>
+        </div>
       </div>
-      <p className="text-center my-4 text-lg">{description}</p>
 
       <ItemsList items={data} />
     </div>
