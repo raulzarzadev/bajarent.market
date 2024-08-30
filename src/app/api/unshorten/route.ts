2cf8b7f925daab81b1994a3ec5e8676af9d183a3
Raul Zarza <raulzarza.dev@gmail.com>
2024-08-30 15:14:15 -0700

    limit the numbers of redirects

--- src/app/api/unshorten/route.ts
+++ src/app/api/unshorten/route.ts
@@ -12,15 +12,22 @@ export async function GET(req: NextRequest) {
   try {
     let currentUrl = url
     let response
+    const maxRedirects = 10 // Limitar el número de redirecciones para evitar bucles infinitos
+    let redirectCount = 0
 
     // Seguir redirecciones manualmente
-    while (true) {
+    while (redirectCount < maxRedirects) {
       response = await fetch(currentUrl, { method: 'HEAD' })
       if (response.redirected) {
         currentUrl = response.url
+        redirectCount++
       } else {
         break
       }
+    }
+
+    if (redirectCount === maxRedirects) {
+      return NextResponse.json({ error: 'Too many redirects' }, { status: 400 })
     }
 
     return NextResponse.json({ finalUrl: currentUrl })
