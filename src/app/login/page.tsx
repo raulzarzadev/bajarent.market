7d8487f97e56ecb603b01ea7b90b78b4f798a2d0
Raul Zarza <raulzarza.dev@gmail.com>
2025-11-11 10:17:15 -0700

    add app in welcom frase

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -25,7 +25,7 @@ export default function Login() {
             />
           </div>
           <h1 className="text-3xl font-bold text-gray-900 mb-2">
-            Bienvenido a BajaRent
+            Bienvenido a BajaRentApp
           </h1>
         </div>
 
